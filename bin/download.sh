eb5d45780ede11d23c90fa28bfc165aabc548d01
Konstantin Golub <key.offecka@runbox.com>
2017-01-26 23:12:23 -0500

    Batch download

---
+++ bin/download.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set +h
+set -e
+set -u
+
+cd "$(dirname $0)"
+
+name=$1
+file="$WORKDIR/share/files/$name.md5"
+
+case "$name" in
+  proto)
+    base="http://ftp.x.org/pub/individual/proto/"
+    ;;
+  lib)
+    base="http://ftp.x.org/pub/individual/lib/"
+esac
+
+mkdir -pv "$REPO_DIR/$name"
+    
+grep -v '^#' "$file" | awk '{print $2}' | wget -i- -c --no-check-certificate -P "$REPO_DIR/$name" -B "$base"
+grep -v '^#' "$file" | sed -r "s,(.+)\s+(.+),\1$REPO_DIR/$name/\2," | md5sum -c -
